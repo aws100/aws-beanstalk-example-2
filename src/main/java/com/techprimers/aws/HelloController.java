c9f6c983464b5b152ebffad7ff2e68d1b8d39d21
aws100 <98050610+aws100@users.noreply.github.com>
2025-04-18 20:51:06 +0530

    Update HelloController.java

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -1,8 +1,8 @@
 package com.techprimers.aws;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import  org.springframework.web.bind.annotation.GetMapping;
+import  org.springframework.web.bind.annotation.RequestMapping;
+import  org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 @RestController
